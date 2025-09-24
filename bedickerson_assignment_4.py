a25234baea5244b2efe2703e5c9e53ba2c05c5e8
bryedickerson <bryedickerson@gmail.com>
2025-09-24 16:01:42 -0400

    Add files via upload

---
+++ bedickerson_assignment_4.py
@@ -0,0 +1,13 @@
+student_name = "Bryant Dickerson"
+current_gpa = 4.0
+study_hours = 6
+social_points = 35
+stress_level = 70
+# social_points was evaluated on a scale of 0-50.
+# stress_level was evaluated on a scale of 0-100.
+
+print(f"Student Name: {student_name}")
+print(f"Current GPA: {current_gpa:.2f}")
+print(f"Study hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}")
